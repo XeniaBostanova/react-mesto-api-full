6c3f1a55593ba276aac834133989112f356f5249
Xenia Bostanova <ksenia.bostanova@gmail.com>
2022-06-20 17:22:57 +0600

    fix: убрать авторизацию по куки

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -111,10 +111,7 @@ const login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'secret-code', { expiresIn: '7d' });
-      res.cookie('jwt', token, {
-        maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
-      });
+
       res.send({ token });
     })
     .catch(next);
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,7 +4,14 @@ const UnauthorizedError = require('../errors/unauthorized-err');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, _, next) => {
-  const token = req.cookies.jwt;
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new UnauthorizedError('Необходима авторизация'));
+  }
+
+  const token = authorization.replace('Bearer ', '');
+
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-code');
