412e43690b466fbfcf96af8a2c78aa95ca03f613
Xenia Bostanova <ksenia.bostanova@gmail.com>
2022-06-21 15:38:50 +0600

    fix: исправить условие card owner

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -47,7 +47,7 @@ function App() {
 
   function handleCardLike(card) {
     // Снова проверяем, есть ли уже лайк на этой карточке
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const isLiked = card.likes.some(i => i._id === currentUser._id || i === currentUser._id);
 
     api.changeLikeCardStatus(card._id, !isLiked)
       .then((newCard) => {
