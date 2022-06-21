2bbb258fc36768e39189b4c3971ba5603d9009d5
Xenia Bostanova <ksenia.bostanova@gmail.com>
2022-06-21 15:43:52 +0600

    fix: исправить условие в isLiked

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -13,7 +13,7 @@ function Card({card, onCardClick, onCardLike, onCardDelete, }) {
   );
 
   // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
-  const isLiked = card.likes.some(i => i._id === currentUser._id);
+  const isLiked = card.likes.some(i => i._id === currentUser._id || i === currentUser._id);
 
   // Создаём переменную, которую после зададим в `className` для кнопки лайка
   const cardLikeButtonClassName = `element__like-button ${isLiked && 'element__like-button_active'}`;
